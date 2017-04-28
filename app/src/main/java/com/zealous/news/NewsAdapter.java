4a6b3361ce3a528dd141d818c598f639ae7f4fe6
yaaminu <afolanyaaminu@gmail.com>
2017-04-28 15:20:02 +0000

    max length of news description is now 100 Use one color for all news outlets

--- app/src/main/java/com/zealous/news/NewsAdapter.java
+++ app/src/main/java/com/zealous/news/NewsAdapter.java
@@ -1,6 +1,5 @@
 package com.zealous.news;
 
-import android.graphics.drawable.Drawable;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.format.DateUtils;
@@ -17,18 +16,10 @@ public class NewsAdapter extends BaseAdapter<NewsItemHolder, NewsItem> {
     final Delegate delegate;
     private final int maxDescriptionLength;
 
-    private final Html.ImageGetter imageGetter;
-
     public NewsAdapter(Delegate delegate) {
         super(delegate);
         this.delegate = delegate;
         maxDescriptionLength = delegate.context().getResources().getInteger(R.integer.max_description_length);
-        imageGetter = new Html.ImageGetter() {
-            @Override
-            public Drawable getDrawable(String source) {
-                return NewsAdapter.this.delegate.context().getResources().getDrawable(R.drawable.empty);
-            }
-        };
     }
 
     @Override
@@ -38,8 +29,7 @@ public class NewsAdapter extends BaseAdapter<NewsItemHolder, NewsItem> {
         holder.datPublished.setText(DateUtils.getRelativeDateTimeString(delegate.context(), item.getDate(),
                 DateUtils.MINUTE_IN_MILLIS, DateUtils.DAY_IN_MILLIS, DateUtils.FORMAT_ABBREV_ALL));
         holder.source.setText(item.getSource());
-//        holder.sourceBar.setBackgroundResource(item.getPublisherColor());
-        Spanned formattedDescription = Html.fromHtml(item.getDescription(), imageGetter, null);
+        Spanned formattedDescription = Html.fromHtml(item.getDescription());
         if (formattedDescription.length() > maxDescriptionLength) {
             holder.description.setText(formattedDescription.subSequence(0, maxDescriptionLength - 3));
             holder.description.append("...");
