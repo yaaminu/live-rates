605229f00ca6835dbcccd25895054a1037da12bb
yaaminu <afolanyaaminu@gmail.com>
2017-04-28 00:26:23 +0000

    hide the images from showing up in descriptions

--- app/src/main/java/com/zealous/news/NewsAdapter.java
+++ app/src/main/java/com/zealous/news/NewsAdapter.java
@@ -1,6 +1,8 @@
 package com.zealous.news;
 
+import android.graphics.drawable.Drawable;
 import android.text.Html;
+import android.text.Spanned;
 import android.text.format.DateUtils;
 import android.view.ViewGroup;
 
@@ -13,10 +15,20 @@ import com.zealous.adapter.BaseAdapter;
 
 public class NewsAdapter extends BaseAdapter<NewsItemHolder, NewsItem> {
     final Delegate delegate;
+    private final int maxDescriptionLength;
+
+    private final Html.ImageGetter imageGetter;
 
     public NewsAdapter(Delegate delegate) {
         super(delegate);
         this.delegate = delegate;
+        maxDescriptionLength = delegate.context().getResources().getInteger(R.integer.max_description_length);
+        imageGetter = new Html.ImageGetter() {
+            @Override
+            public Drawable getDrawable(String source) {
+                return NewsAdapter.this.delegate.context().getResources().getDrawable(R.drawable.empty);
+            }
+        };
     }
 
     @Override
@@ -27,7 +39,13 @@ public class NewsAdapter extends BaseAdapter<NewsItemHolder, NewsItem> {
                 DateUtils.MINUTE_IN_MILLIS, DateUtils.DAY_IN_MILLIS, DateUtils.FORMAT_ABBREV_ALL));
         holder.source.setText(item.getSource());
         holder.sourceBar.setBackgroundResource(item.getPublisherColor());
-        holder.description.setText(Html.fromHtml(item.getDescription()));
+        Spanned formattedDescription = Html.fromHtml(item.getDescription(), imageGetter, null);
+        if (formattedDescription.length() > maxDescriptionLength) {
+            holder.description.setText(formattedDescription.subSequence(0, maxDescriptionLength - 3));
+            holder.description.append("...");
+        } else {
+            holder.description.setText(formattedDescription);
+        }
     }
 
     @Override
