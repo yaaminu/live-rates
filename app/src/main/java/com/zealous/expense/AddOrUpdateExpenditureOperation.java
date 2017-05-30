e2ddd30832da5f8c9a53a6040718b6d6c8209a7a
yaaminu <afolanyaaminu@gmail.com>
2017-05-30 14:27:59 +0000

    extract common behaviours in AddOrUpdateExpenditureOperation into a separate parent class
    update AddOrUpdateExpenditureOperation accordingly

--- app/src/main/java/com/zealous/expense/AddOrUpdateExpenditureOperation.java
+++ app/src/main/java/com/zealous/expense/AddOrUpdateExpenditureOperation.java
@@ -1,10 +1,8 @@
 package com.zealous.expense;
 
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
 import com.backup.BackupException;
-import com.backup.Operation;
 import com.google.gson.JsonObject;
 import com.zealous.utils.PLog;
 
@@ -12,14 +10,10 @@ import com.zealous.utils.PLog;
  * Created by yaaminu on 5/30/17.
  */
 
-public class AddOrUpdateExpenditureOperation implements Operation {
+public class AddOrUpdateExpenditureOperation extends BaseExpenditureOperation {
     private static final String TAG = "AddOrUpdateExpenditureOperation";
-    //will be injected
-    @Nullable
-    ExpenditureDataSource dataSource;
-    @NonNull
-    private JsonObject data;
 
+    //required no-arg constructor
     public AddOrUpdateExpenditureOperation() {
         data = new JsonObject();
     }
@@ -28,28 +22,9 @@ public class AddOrUpdateExpenditureOperation implements Operation {
         data = expenditure.toJson();
     }
 
-
-    @NonNull
     @Override
-    public JsonObject data() {
-        return data;
-    }
-
-    @Override
-    public void setData(@NonNull JsonObject object) {
-        this.data = object;
-    }
-
-    @Override
-    public void replay() throws BackupException {
+    protected void doReplay() throws BackupException {
         PLog.d(TAG, "replaying operation:  %s", getClass().getName());
-        if (dataSource == null) {
-            throw new IllegalStateException("data source is null, did you forget to  inject it?");
-        }
-        //noinspection ConstantConditions
-        if (data == null) {
-            throw new IllegalStateException("data  is null, did you forget to  inject it?");
-        }
         Expenditure expenditure = Expenditure.fromJson(data);
         assert dataSource != null;
         dataSource.addOrUpdateExpenditure(expenditure);
---
+++ app/src/main/java/com/zealous/expense/BaseExpenditureOperation.java
@@ -0,0 +1,52 @@
+package com.zealous.expense;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.backup.BackupException;
+import com.backup.Operation;
+import com.google.gson.JsonObject;
+
+/**
+ * Created by yaaminu on 5/30/17.
+ */
+
+public class BaseExpenditureOperation implements Operation {
+
+    //will be injected
+    @Nullable
+    ExpenditureDataSource dataSource;
+
+    protected JsonObject data;
+
+    public BaseExpenditureOperation() {
+        data = new JsonObject();
+    }
+
+    @NonNull
+    @Override
+    public final JsonObject data() {
+        return data;
+    }
+
+    @Override
+    public final void setData(@NonNull JsonObject data) {
+        this.data = data;
+    }
+
+    protected void doReplay() throws BackupException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final void replay() throws BackupException {
+        if (dataSource == null) {
+            throw new IllegalStateException("data source is null, did you forget to  inject it?");
+        }
+        //noinspection ConstantConditions
+        if (data == null) {
+            throw new IllegalStateException("data  is null, did you forget to  inject it?");
+        }
+        doReplay();
+    }
+}
