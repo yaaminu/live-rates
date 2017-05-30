0a9195e74b3e372c87f81d6a6b809e5932bb4a12
yaaminu <afolanyaaminu@gmail.com>
2017-05-30 11:02:33 +0000

    GoogleDriveStorage is complete but It's yet to be tested added expresso as a test dependency

---
+++ backup/src/androidTest/java/GoogleDriveStorageTest.java
@@ -0,0 +1,42 @@
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.fail;
+
+/**
+ * Created by yaaminu on 5/30/17.
+ */
+@RunWith(AndroidJUnit4.class)
+public class GoogleDriveStorageTest {
+    @Test
+    public void newAppendableOutPutStream() throws Exception {
+        fail("not implemented");
+    }
+
+    @Test
+    public void newInputStream() throws Exception {
+        fail("not implemented");
+    }
+
+    @Test
+    public void lastModified() throws Exception {
+        fail("not implemented");
+    }
+
+    @Test
+    public void size() throws Exception {
+        fail("not implemented");
+    }
+
+    @Test
+    public void sync() throws Exception {
+        fail("not implemented");
+    }
+
+    @Test
+    public void initialized() throws Exception {
+        fail("not implemented");
+    }
+}
--- backup/src/main/java/com/backup/GoogleDriveStorage.java
+++ backup/src/main/java/com/backup/GoogleDriveStorage.java
@@ -1,36 +1,35 @@
 package com.backup;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Looper;
 
 import com.android.annotations.Nullable;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveContents;
 import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveFolder;
-import com.google.android.gms.drive.Metadata;
 import com.google.android.gms.drive.MetadataBuffer;
+import com.google.android.gms.drive.MetadataChangeSet;
 import com.google.android.gms.drive.query.Filter;
 import com.google.android.gms.drive.query.Filters;
 import com.google.android.gms.drive.query.Query;
 import com.google.android.gms.drive.query.SearchableField;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.Semaphore;
 
 import static android.content.Context.MODE_APPEND;
-import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by yaaminu on 5/20/17.
@@ -38,11 +37,8 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class GoogleDriveStorage implements Storage {
 
+    public static final String APP_BACKUP_SUFFIX = ".app-backup";
     private static final String DRIVE_BACKUP_BACKUPLOG_DIRNAME = "drive_backup.waitig";
-    private static final String PREF_DRIVE_BACKUP = "drive_backup";
-    private static final String KEY_LOCAL_BACKUP_CURSOR_PREFIX = "backup.pending.cursor.";
-    private static final Filter BACKUP_FOLDER_FILTER = Filters.eq(SearchableField.TITLE, "app-backup");
-    private static final String BACKUP_CURSOR_PREFIX = "backup.cursor.";
     private static final InputStream EMPTY_INPUT_STREAM = new InputStream() {
         @Override
         public int read() throws IOException {
@@ -50,7 +46,6 @@ public class GoogleDriveStorage implements Storage {
         }
     };
     private final Semaphore lock;
-    private final SharedPreferences preferences;
     private final LocalFileSystemStorage storage;
 
     @Nullable
@@ -59,12 +54,11 @@ public class GoogleDriveStorage implements Storage {
 
     public GoogleDriveStorage(Context context) {
         lock = new Semaphore(1, true);
-        preferences = context.getSharedPreferences(PREF_DRIVE_BACKUP, MODE_PRIVATE);
         this.storage = new LocalFileSystemStorage(
                 context.getDir(DRIVE_BACKUP_BACKUPLOG_DIRNAME, MODE_APPEND));
     }
 
-    private void initiaize(Context context) {
+    public void initiaize(Context context) {
         if (Looper.getMainLooper().getThread() == Thread.currentThread()) {
             throw new IllegalStateException("can't make this call on the main thread");
         }
@@ -83,28 +77,23 @@ public class GoogleDriveStorage implements Storage {
     @Override
     public OutputStream newAppendableOutPutStream(String collectionName) throws IOException {
         lock.acquireUninterruptibly();
-        if (outputStream == null) {
-            outputStream = storage.newAppendableOutPutStream(collectionName);
-        }
-        return new OutputStream() {
-            @Override
-            public void write(int b) throws IOException {
-                outputStream.write(b);
+        try {
+            if (outputStream == null) {
+                outputStream = storage.newAppendableOutPutStream(collectionName);
             }
 
-            @Override
-            public void close() throws IOException {
-                if (outputStream != null) {
-                    outputStream.close();
-                    outputStream = null;
-                }
-                lock.release(); //release the lock  here
-            }
-        };
+            //wrap the outputStream in another output stream so we
+            //can play well with the lock.
+            return new LockAwareOutPutStream(outputStream, lock);
+        } catch (Throwable e) {
+            lock.release();
+            throw e;
+        }
     }
 
     @Override
     public InputStream newInputStream(String collectionName) throws IOException {
+        MetadataBuffer metadataBuffer = null;
         try {
             lock.acquireUninterruptibly();
             if (apiClient == null) {
@@ -113,48 +102,84 @@ public class GoogleDriveStorage implements Storage {
             DriveFolder folder = Drive.DriveApi.getAppFolder(apiClient);
             PendingResult<DriveApi.MetadataBufferResult> resultPendingResult =
                     folder.queryChildren(apiClient, new Query.Builder()
-                            .addFilter(BACKUP_FOLDER_FILTER)
+                            .addFilter(Filters.eq(SearchableField.TITLE, collectionName + APP_BACKUP_SUFFIX))
                             .build());
-            MetadataBuffer result =
-                    resultPendingResult.await().getMetadataBuffer();
-            if (result.getCount() == 0) {
+
+            DriveApi.MetadataBufferResult results = resultPendingResult.await();
+            if (!results.getStatus().isSuccess()) {
+                throw new IOException(results.getStatus().getStatusMessage());
+            }
+
+            metadataBuffer =
+                    results.getMetadataBuffer();
+
+            if (metadataBuffer.getCount() == 0) {
                 //first time.../ return nothing
                 return EMPTY_INPUT_STREAM;
             }
-            List<DriveFile> files = new ArrayList<>(result.getCount());
-
-            for (Metadata metadata : result) {
-                DriveFile file = metadata.getDriveId().asDriveFile();
-                long cursor = preferences.getLong(BACKUP_CURSOR_PREFIX + file.getDriveId(), 0);
-
-                if (cursor < metadata.getFileSize()) { //we have something to backup
-                    files.add(file);
-                }
+            DriveFile file = metadataBuffer.get(0).getDriveId().asDriveFile();
+            DriveApi.DriveContentsResult driveContentResults = file.open(apiClient, DriveFile.MODE_READ_ONLY, null)
+                    .await();
+            if (!driveContentResults.getStatus().isSuccess()) {
+                throw new IOException(driveContentResults.getStatus().getStatusMessage());
             }
-            if (files.isEmpty()) {
-                return EMPTY_INPUT_STREAM;
-            }
-            return new DriveFilesInputStream(apiClient, files);
-        } finally {
+            //wrap the InputStream with another input stream so we can play
+            //well with the lock
+            return new LockAwareInputStream(driveContentResults
+                    .getDriveContents().getInputStream(), lock);
+        } catch (Throwable e) { //catch all exceptions and release lock, rethrow after that
             lock.release();
+            if (metadataBuffer != null) {
+                metadataBuffer.release();
+            }
+            throw e;
         }
     }
 
     @Override
     public long size(String collectionName) throws IOException {
+        MetadataBuffer metadataBuffer = null;
         try {
             lock.acquireUninterruptibly();
-            return 0;
+            DriveApi.MetadataBufferResult result = Drive.DriveApi.getAppFolder(apiClient)
+                    .queryChildren(apiClient,
+                            new Query.Builder()
+                                    .addFilter(Filters.eq(SearchableField.TITLE,
+                                            collectionName + APP_BACKUP_SUFFIX)).build()).await();
+            throwIfNotSuccess(result.getStatus());
+
+            metadataBuffer = result.getMetadataBuffer();
+            if (metadataBuffer.getCount() == 0) {
+                return 0;
+            }
+            return metadataBuffer.get(0).getFileSize();
         } finally {
+            if (metadataBuffer != null) {
+                metadataBuffer.release();
+            }
             lock.release();
         }
     }
 
     @Override
-    public long lastModified(String collectionName) {
+    public long lastModified(String collectionName) throws IOException {
         try {
             lock.acquireUninterruptibly();
-            return 0;
+            DriveApi.MetadataBufferResult result = Drive.DriveApi.getAppFolder(apiClient)
+                    .queryChildren(apiClient,
+                            new Query.Builder()
+                                    .addFilter(Filters.eq(SearchableField.TITLE,
+                                            collectionName + APP_BACKUP_SUFFIX)).build()).await();
+            MetadataBuffer metadataBuffer = result.getMetadataBuffer();
+            try {
+                throwIfNotSuccess(result.getStatus());
+                if (metadataBuffer.getCount() == 0) {
+                    return 0;
+                }
+                return metadataBuffer.get(0).getModifiedDate().getTime();
+            } finally {
+                metadataBuffer.release();
+            }
         } finally {
             lock.release();
         }
@@ -163,26 +188,82 @@ public class GoogleDriveStorage implements Storage {
     public void sync(final String collectionName) throws BackupException {
         try {
             lock.acquireUninterruptibly();
-            long cursor = preferences.getLong(KEY_LOCAL_BACKUP_CURSOR_PREFIX + collectionName, -1);
-            if (cursor == -1) {
-                //got nothing to backup
-                return;
-            }
-            //backup the temporary log, delete it and  update the cursor to -1.
-            //also check for the log of other devices, pull them and apply the changes locally.
+
+
+            //TODO also check for the log of other devices, pull them and apply the changes locally.
 
             //we are dead sure that no one is modifying the temporary backup file now
-            //se we have the lock.
-            File file = storage.getBackupFile(collectionName);
-            final File copy = new File(file.getParentFile(), file.getName() + "-copy~");
-            FileUtils.copyFile(file, copy);
-            // TODO: 5/22/17 push the content of copy to google drive
-            // TODO: 5/22/17 update the cursor
-            throw new UnsupportedOperationException();
+            //since we hold the lock.
+            File copy = storage.getBackupFile(collectionName);
+            if (!copy.exists() || copy.length() == 0) { //we don't have a pending backup
+                System.out.println("back up complete");
+                return;
+            }
+            Filter filter = Filters.eq(SearchableField.TITLE, collectionName + APP_BACKUP_SUFFIX);
+            DriveApi.MetadataBufferResult result = Drive.DriveApi.getAppFolder(apiClient)
+                    .queryChildren(apiClient, new Query.Builder()
+                            .addFilter(filter).build())
+                    .await();
+            throwIfNotSuccess(result.getStatus());
+            MetadataBuffer metadataBuffer = result.getMetadataBuffer();
+            try {
+                if (metadataBuffer.getCount() == 0) {
+                    //create the file
+                    DriveApi.DriveContentsResult driveContentsResult = Drive.DriveApi.newDriveContents(apiClient).await();
+                    throwIfNotSuccess(driveContentsResult.getStatus());
+
+                    DriveContents driveContents = driveContentsResult.getDriveContents();
+                    OutputStream localOutStream = driveContents.getOutputStream();
+                    InputStream in = new FileInputStream(copy);
+
+                    copy(in, localOutStream);
+
+                    MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                            .setTitle(collectionName + APP_BACKUP_SUFFIX).build();
+                    DriveFolder.DriveFileResult driveFileResult = Drive.DriveApi.getAppFolder(apiClient)
+                            .createFile(apiClient, changeSet
+                                    , driveContents).await();
+                    throwIfNotSuccess(driveFileResult.getStatus());
+
+                } else { //file exist
+                    DriveFile backupFile = metadataBuffer.get(0).getDriveId().asDriveFile();
+                    DriveApi.DriveContentsResult contentsResult = backupFile.open(apiClient, DriveFile.MODE_WRITE_ONLY, null)
+                            .await();
+                    throwIfNotSuccess(contentsResult.getStatus());
+                    OutputStream localOutStream = contentsResult.getDriveContents().getOutputStream();
+                    copy(new FileInputStream(copy), localOutStream);
+                }
+
+                if (!copy.delete()) {
+                    System.err.println("failed to delete backed up copy");
+                }
+            } finally {
+                metadataBuffer.release();
+            }
         } catch (IOException e) {
             throw new BackupException(BackupException.EIOERROR, e.getMessage(), e);
         } finally {
             lock.release();
+        }
+    }
+
+    private void throwIfNotSuccess(Status status) throws IOException {
+        if (!status.isSuccess()) {
+            throw new IOException(status.getStatusMessage());
+        }
+    }
+
+    private void copy(InputStream in, OutputStream out) throws IOException {
+        try {
+            byte[] buff = new byte[4096];
+            int read;
+            while ((read = in.read(buff)) != -1) {
+                out.write(buff, 0, read);
+            }
+            out.flush();
+        } finally {
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(out);
         }
     }
 }
