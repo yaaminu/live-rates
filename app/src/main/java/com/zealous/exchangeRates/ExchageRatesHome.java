c432a894865296720bb396c4abe3f40f269df28c
Aminu Abdul Manaf <afolanyaaminu@gmail.com>
2018-03-25 07:06:35 +0000

    optimize imports

--- app/src/main/java/com/zealous/exchangeRates/ExchageRatesHome.java
+++ app/src/main/java/com/zealous/exchangeRates/ExchageRatesHome.java
@@ -14,6 +14,7 @@ import com.zealous.adapter.SimpleListItemHolder;
 import com.zealous.adapter.SimpleRecyclerViewAdapter;
 import com.zealous.ui.BaseFragment;
 
+import java.util.HashSet;
 import java.util.List;
 
 import butterknife.BindView;
