485702b15134d2246105e458c24ed28fbe0a052f
Aminu Abdul Manaf <afolanyaaminu@gmail.com>
2018-04-08 08:35:59 +0000

    Improve design of equity details screen

--- app/src/main/java/com/zealous/equity/EquityBasicInfoFragment.kt
+++ app/src/main/java/com/zealous/equity/EquityBasicInfoFragment.kt
@@ -23,24 +23,24 @@ class EquityBasicInfoFragment : BaseFragment() {
         viewModel
                 .getItem().observe(this, Observer { equity: Equity? ->
 
-            tv_company_name.text = equity!!.name
-            tv_change.text = equity.change
-            tv_price.text = equity.price
-            tv_symbol.text = equity.symbol
-            ib_fav_equity.setImageResource(if (equity.isFavorite) R.drawable.ic_notifications_active_black_24dp else R.drawable.ic_notifications_none_black_24dp)
+                    tv_company_name.text = equity!!.name
+                    tv_change.text = equity.change
+                    tv_price.text = equity.price
+                    tv_symbol.text = equity.symbol
+                    ib_fav_equity.setImageResource(if (equity.isFavorite) R.drawable.ic_notifications_active_black_24dp else R.drawable.ic_notifications_none_black_24dp)
 
-            _24_hour_high.text = Html.fromHtml("<b>24 Hour Low:</b>  ${ExchangeRate.FORMAT.format(equity._24hrLo)}")
-            _24_hour_low.text = Html.fromHtml("<b>24 Hour High:</b> ${ExchangeRate.FORMAT.format(equity._24hrHi)}")
-            market_cap.text = Html.fromHtml("<b>Market Cap:</b>   ${ExchangeRate.FORMAT.format(equity.marketCap)}")
-            open.text = Html.fromHtml("<b>Today Open:</b>  ${ExchangeRate.FORMAT.format(equity.marketOpen)}")
+                    _24_hour_high.text = Html.fromHtml("<b>24 Hour Low:</b>  ${ExchangeRate.FORMAT.format(equity._24hrLo)}")
+                    _24_hour_low.text = Html.fromHtml("<b>24 Hour High:</b> ${ExchangeRate.FORMAT.format(equity._24hrHi)}")
+                    market_cap.text = Html.fromHtml("<b>Market Cap:</b>   ${ExchangeRate.FORMAT.format(equity.marketCap)}")
+                    open.text = Html.fromHtml("<b>Today Open:</b>  ${ExchangeRate.FORMAT.format(equity.marketOpen)}")
 
-            back.setOnClickListener {
-                activity.finish()
-            }
+                    back.setOnClickListener {
+                        activity.finish()
+                    }
 
-            ib_fav_equity.setOnClickListener {
-                viewModel.updateFavorite(equity)
-            }
-        })
+                    ib_fav_equity.setOnClickListener {
+                        viewModel.updateFavorite(equity)
+                    }
+                })
     }
 }
